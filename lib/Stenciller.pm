57952cbbc1a99e3dda109e33dcd48953109d517c
Erik Carlsson <info@code301.com>
2015-01-14 14:01:02 +0100

    Init

---
+++ lib/Stenciller.pm
@@ -0,0 +1,29 @@
+use 5.10.1;
+use strict;
+use warnings;
+
+package Stenciller;
+
+# VERSION
+# ABSTRACT: Short intro
+
+
+
+1;
+
+
+__END__
+
+=pod
+
+=head1 SYNOPSIS
+
+    use Stenciller;
+
+=head1 DESCRIPTION
+
+Stenciller is ...
+
+=head1 SEE ALSO
+
+=cut
---
+++ t/01-basic.t
@@ -0,0 +1,11 @@
+use strict;
+use warnings FATAL => 'all';
+use Test::More;
+use if $ENV{'AUTHOR_TESTING'}, 'Test::Warnings';
+
+use Stenciller;
+
+# replace with the actual test
+ok 1;
+
+done_testing;
